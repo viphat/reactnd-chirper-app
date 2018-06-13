1679b92627626f862dbcabf1ab6baaa2031aa260
Duong Vi Phat <viphat@gmail.com>
2018-06-13 16:12:25 +0700

    Reducers & combineReducers

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,8 +1,9 @@
 import { getInitialData } from '../utils/api'
-
 import { receiveUsers } from './users'
-
 import { receiveTweets } from './tweets'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
   return (dispatch) => {
@@ -10,6 +11,7 @@ export function handleInitialData() {
       .then(({users, tweets}) => {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
+        dispatch(setAuthedUser(AUTHED_ID))
       })
   }
 }
