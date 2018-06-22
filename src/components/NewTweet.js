b62e6abb63df11949c1af34da4d13355f0072492
Duong Vi Phat <viphat@gmail.com>
2018-06-22 09:53:08 +0700

    Using React Router

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleAddTweet } from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
 
 class NewTweet extends Component {
   state = {
     text: '',
+    toHome: false,
   }
 
   handleChange = (e) => {
@@ -24,12 +26,18 @@ class NewTweet extends Component {
     dispatch(handleAddTweet(text, id))
 
     this.setState(() => ({
-      text: ''
+      text: '',
+      toHome: id ? false : true,
     }))
   }
 
   render() {
-    const { text } = this.state
+    const { text, toHome } = this.state
+
+    if (toHome === true) {
+      return <Redirect to='/' />
+    }
+
     const tweetLeft = 256 - text.length
 
     return (
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -21,12 +22,11 @@ class Tweet extends Component {
 
   toParent = (e, id) => {
     e.preventDefault()
+    this.props.history.push(`/tweet/${id}`)
     // Todo: Redirect to parent Tweet.
   }
 
   render() {
-    console.log(this.props)
-
     const { tweet } = this.props
 
     if (tweet === null) {
@@ -38,7 +38,7 @@ class Tweet extends Component {
     } = tweet
 
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img src={avatar} alt={`Avatar of ${name}`} className='avatar' />
         <div className='tweet-info'>
           <div>
@@ -62,7 +62,7 @@ class Tweet extends Component {
             <span>{ likes !== 0 && likes }</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -79,4 +79,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
